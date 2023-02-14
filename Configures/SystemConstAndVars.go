6aa1154ebfb542be039cbb26043d4cd58bc2a1cf
nmaslenko <nvmaslenko@gmail.com>
2023-02-14 15:05:39 +0500

    + readme, подготовка для компиляции в deb

--- Configures/SystemConstAndVars.go
+++ Configures/SystemConstAndVars.go
@@ -2,10 +2,10 @@ package configures
 
 const (
 	IPPattern                      = `[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}`
-	DefaultSettingsPath            = "Default/Settings.yaml"
+	DefaultSettingsPath            = "/bin/ktsnet/Default/Settings.yaml"
 	NetInterfaceManualSettingsHelp = "- ручной выбор сетевого интерфейса для замены подсети."
 	SubnetSettingsHelp             = "- настройка подсети. Введите номер подсети (1-255)."
 	InterfacesPathManualHelp       = "- абсолютный путь до файла с описанием и настройками сетевых интерфейсов."
-	NetworkingServiceCall          = "/nit.d/networking"
+	NetworkingServiceCall          = "/etc/init.d/networking"
 	RestartArg                     = "restart"
 )
